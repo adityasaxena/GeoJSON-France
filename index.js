d4af2f595800fade792dc60e3119b8036268991b
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2014-12-28 19:34:52 +0530

    Sending json response via nodejs server reading a json file and using gzip compression

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+var fs = require("fs");
+var file = JSON.parse(fs.readFileSync("communes.geojson", "utf8"));
 var express = require('express');
 var app = express();
+var compress = require('compression');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(compress());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/data', function(request, response) {
+  response.send(file);
 });
 
 app.listen(app.get('port'), function() {
